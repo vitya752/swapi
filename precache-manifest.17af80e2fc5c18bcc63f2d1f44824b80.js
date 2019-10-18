3859746c8de90ca555f19942d1a3821cf0d8a201
vitya752 <vitya752@ukr.net>
2019-10-18 18:36:08 +0300

    Updates

---
+++ precache-manifest.17af80e2fc5c18bcc63f2d1f44824b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9485b20cb0df7450ad8abed5b5bc91",
+    "url": "/swapi/index.html"
+  },
+  {
+    "revision": "2d10c890abcba8f0b385",
+    "url": "/swapi/static/css/main.ac448e54.chunk.css"
+  },
+  {
+    "revision": "b61e0f392384bc46f353",
+    "url": "/swapi/static/js/2.d4d3e8da.chunk.js"
+  },
+  {
+    "revision": "2d10c890abcba8f0b385",
+    "url": "/swapi/static/js/main.95509cb0.chunk.js"
+  },
+  {
+    "revision": "cd759bdc63c98b68b183",
+    "url": "/swapi/static/js/runtime-main.0ceb0c54.js"
+  }
+]);
